feef1cd827e85f361545889eae4eddf4d8e3da13
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-08 13:44:36 -0600

    test: added user story 1

---
+++ relational_rails/spec/features/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'areas index page' do
+  describe 'areas can be displayed to users' do
+    it 'can show each area available to a user' do
+      # For each parent table
+      # As a visitor
+      # When I visit '/parents'
+      # Then I see the name of each parent record in the system
+    end
+  end
+end
