fd42b433513720d9dffcfb16eed901428005d077
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-12 19:09:44 -0600

    test: link to all areas

--- relational_rails/spec/features/navigation_spec.rb
+++ relational_rails/spec/features/navigation_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe "Navigation" do
   it "displays a link to the routes index on every page" do
     area1 = Area.create(name: "Animal World", rock_type: "Granite", state: "Colorado", county: "Boulder", approach_distance: 30, roadside_access: false)
-    visit "/areas/(#{area1.id}"
+    visit "/areas"
 
-    expect(page).to have_link("Routes", href: )
+    expect(page).to have_link("All Routes", href: routes_path)
+  end
+
+  it 'displays a link to the areas on every page' do
+    visit "/routes"
+
+    expect(page).to have_link("All Areas", href: areas_path)
   end
 end
 
